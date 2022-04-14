7e102cdeaa889867a4828588b9ad2dab1e025ad6
Jeremias Tardivo <jeretardivo1992@gmail.com>
2022-04-13 21:03:43 -0300

    Tailwindcss add

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>PROXIMAMENTE</h1>
+        <h1 className="text-3xl font-bold underline">
+          Proximamente
+        </h1>
         <p>
           Ecommerce del C.D.R.E
         </p>
+
       </header>
     </div>
   );
