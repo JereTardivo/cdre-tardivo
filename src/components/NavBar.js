8affb7e4d64187edfd6db6fd90ca14813a96a212
Jeremias Tardivo <jeretardivo1992@gmail.com>
2022-05-07 19:48:03 -0300

    navbajlateral

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import CartWidget from './CartWidget'
 
-const NavBar2 = () => {
+const NavBar = () => {
     return (
         <>
             <div className="relative bg-white" >
@@ -14,8 +14,8 @@ const NavBar2 = () => {
                             </Link>
                         </div>
                         <div className="-mr-2 -my-2 md:hidden">
-                            <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
-                                <span className="sr-only">Open menu</span>
+                            <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500" aria-expanded="false">
+                                <span className="sr-only">Abrir Menu</span>
                                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                                 </svg>
@@ -25,15 +25,13 @@ const NavBar2 = () => {
                             <div className="relative">
                                 <ul className="menu menu-horizontal p-0">
                                     <li tabIndex="0">
-                                        <Link to='/' type="button" className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false">Shop</Link>
+                                        <Link to='/' type="button" className="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" aria-expanded="false">Shop</Link>
                                         <ul className="p-2 absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2" aria-hidden="true">
                                             <ul className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                                                 <ul className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                                                     <li>
                                                         <Link to={`/category/Remeras`} href="/#" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                                            <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                                            </svg>
+                                                            <img className="flex-shrink-0 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" src="https://i.ibb.co/Wv2rJBf/remera-Icono-Nav-Bar.png" alt="remera-Icono-Nav-Bar" border="0" />
                                                             <div className="ml-4">
                                                                 <p className="text-base font-medium text-gray-900">Remeras</p>
                                                             </div>
@@ -41,9 +39,7 @@ const NavBar2 = () => {
                                                     </li>
                                                     <li>
                                                         <Link to={`/category/Camisas`} href="/#" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                                            <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                                                            </svg>
+                                                            <img className="flex-shrink-0 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" src="https://i.ibb.co/N3M3q8w/camisa-Icono-Nav-Bar.png" alt="camisa-Icono-Nav-Bar" border="0" />
                                                             <div className="ml-4">
                                                                 <p className="text-base font-medium text-gray-900">Camisas</p>
                                                             </div>
@@ -51,9 +47,7 @@ const NavBar2 = () => {
                                                     </li>
                                                     <li>
                                                         <Link to={`/category/Sweaters`} href="/#" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                                            <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                                            </svg>
+                                                            <img className="flex-shrink-0 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" src="https://i.ibb.co/PzCyrSh/sweater-Icono-Nav-Bar.png" alt="sweater-Icono-Nav-Bar" border="0" />
                                                             <div className="ml-4">
                                                                 <p className="text-base font-medium text-gray-900">Sweaters</p>
                                                             </div>
@@ -61,9 +55,7 @@ const NavBar2 = () => {
                                                     </li>
                                                     <li>
                                                         <Link to={`/category/Pantalones`} href="/#" className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                                            <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                                                            </svg>
+                                                            <img className="flex-shrink-0 h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true" src="https://i.ibb.co/Q68Qjv2/pantalon-Icono-Nav-Bar.png" alt="pantalon-Icono-Nav-Bar" border="0" />
                                                             <div className="ml-4">
                                                                 <p className="text-base font-medium text-gray-900">Pantalones</p>
                                                             </div>
@@ -74,10 +66,10 @@ const NavBar2 = () => {
                                         </ul>
                                     </li>
                                     <li tabIndex="0">
-                                        <a href="/#" className="text-base font-medium text-gray-500 hover:text-gray-900"> Nosotros </a>
+                                        <a href="/#" className="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"> Nosotros </a>
                                     </li>
                                     <li tabIndex="0">
-                                        <a href="/#" className="text-base font-medium text-gray-500 hover:text-gray-900"> Contacto </a>
+                                        <a href="/#" className="text-base font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"> Contacto </a>
                                     </li>
                                 </ul>
                             </div>
@@ -103,17 +95,16 @@ const NavBar2 = () => {
                         </div>
                     </div>
                 </div>
-                <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                <div className=" top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
                     <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                         <div className="pt-5 pb-6 px-5">
                             <div className="flex items-center justify-between">
                                 <div>
-                                    <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+                                    <img className="h-8 w-auto" src="https://i.ibb.co/XsnxtkY/Dise-o-sin-t-tulo.png" alt="Dise-o-sin-t-tulo" border="0" />
                                 </div>
                                 <div className="-mr-2">
-                                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                        <span className="sr-only">Close menu</span>
-
+                                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500">
+                                        <span className="sr-only">Cerrar Menu</span>
                                         <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                         </svg>
@@ -122,70 +113,18 @@ const NavBar2 = () => {
                             </div>
                             <div className="mt-6">
                                 <nav className="grid gap-y-8">
-                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-
-                                        <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                        </svg>
-                                        <span className="ml-3 text-base font-medium text-gray-900"> Analytics </span>
+                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">                                        
+                                        <span className="ml-3 text-base font-medium text-gray-900"> Shop </span>
                                     </a>
-
                                     <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-
-                                        <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                                        </svg>
-                                        <span className="ml-3 text-base font-medium text-gray-900"> Engagement </span>
+                                        <span className="ml-3 text-base font-medium text-gray-900"> Nosotros </span>
                                     </a>
-
-                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-
-                                        <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                        </svg>
-                                        <span className="ml-3 text-base font-medium text-gray-900"> Security </span>
-                                    </a>
-
-                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-
-                                        <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                                        </svg>
-                                        <span className="ml-3 text-base font-medium text-gray-900"> Integrations </span>
-                                    </a>
-
-                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-
-                                        <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" aria-hidden="true">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                                        </svg>
-                                        <span className="ml-3 text-base font-medium text-gray-900"> Automations </span>
-                                    </a>
+                                    <a href="/#" className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">    
+                                        <span className="ml-3 text-base font-medium text-gray-900"> Conacto </span>
+                                    </a>                                   
                                 </nav>
                             </div>
-                        </div>
-                        <div className="py-6 px-5 space-y-6">
-                            <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Pricing </a>
-
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Docs </a>
-
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Help Center </a>
-
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Guides </a>
-
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Events </a>
-
-                                <a href="/#" className="text-base font-medium text-gray-900 hover:text-gray-700"> Security </a>
-                            </div>
-                            <div>
-                                <a href="/#" className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
-                                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                                    Existing customer?
-                                    <a href="/#" className="text-indigo-600 hover:text-indigo-500"> Sign in </a>
-                                </p>
-                            </div>
-                        </div>
+                        </div> 
                     </div>
                 </div>
             </div>
@@ -194,4 +133,4 @@ const NavBar2 = () => {
     )
 }
 
-export default NavBar2
+export default NavBar
